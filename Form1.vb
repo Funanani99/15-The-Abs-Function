da99b96f370c251d4fac5add41bb00715bbfd6eb
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-10 09:25:43 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Reflection.Metadata.BlobBuilder
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Dim number, abs As Double
+
+        ' number = Val(TextBox2.Text)
+        ' abs = Math.Abs(number)
+        'TextBox1.Text = abs
+
+        TextBox1.Text = Math.Abs(Val(TextBox2.Text))
+    End Sub
+End Class
